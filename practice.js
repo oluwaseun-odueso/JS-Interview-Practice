1b17f9a66500ea273465d5804b19d79ecd46e366
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2022-10-20 15:03:28 +0100

    prime factors of a number

--- practice.js
+++ practice.js
@@ -1,10 +1,21 @@
-function verifyPrimeNumber(number) {
-    for (let divisor = 2; divisor < number; divisor++) {
-        if (number % divisor == 0) {
-            return false
+// function verifyPrimeNumber(number) {
+//     for (let divisor = 2; divisor < number; divisor++) {
+//         if (number % divisor == 0) {
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+function findPrimeFactors(number) {
+    var primeFactors = []
+    for (let i = 2; number > 2; i++) {
+        if (number % i == 0) {
+            primeFactors.push(i)
+            number = number / i
         }
     }
-    return true
+    return primeFactors
 }
 
-console.log(verifyPrimeNumber(11))
+console.log(findPrimeFactors(20))
