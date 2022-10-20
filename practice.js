e4f046748faf91c80564e6f4bd49e0b59962d476
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2022-10-20 15:52:41 +0100

    greatest common divisor

--- practice.js
+++ practice.js
@@ -7,14 +7,24 @@
 //     return true
 // }
 
-function nthFibonacciNumber(n) {
-    var fibo = [1, 1]
+// function nthFibonacciNumber(n) {
+//     var fibo = [1, 1]
 
-    if (n <= 2) return 1
-    for (let i = 2; i <= n; i++) {
-        fibo[i] = fibo[i-1] + fibo[i-2]
+//     if (n <= 2) return 1
+//     for (let i = 2; i <= n; i++) {
+//         fibo[i] = fibo[i-1] + fibo[i-2]
+//     }
+//     return fibo[n]
+// }
+
+function greatestCommonDivisor(m, n) {
+    var divisor = 0
+    for (let i = 1; i <= m || i <= n ; i++) {
+        if (m % i == 0 && n % i == 0) {
+            divisor = i
+        }
     }
-    return fibo[n]
+    return divisor
 }
 
-console.log(nthFibonacciNumber(12))
+console.log(greatestCommonDivisor(69, 169))
